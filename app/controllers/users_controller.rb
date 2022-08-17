239fc254dec8735bfe8fadda99b2c1dd9a813dc0
takeda_ryoya <t.ryooya@gmail.com>
2022-08-17 12:15:01 +0900

    Add: ユーザーの新規登録機能を追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :tel, :email, :password, :password_confirmation)
+  end
+end
