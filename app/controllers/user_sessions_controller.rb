762b6fb499519d3f07fa2920f313ec8e70f2f28c
takeda_ryoya <t.ryooya@gmail.com>
2022-08-17 19:10:12 +0900

    Add: ユーザーのログイン機能を追加

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,13 @@
+class UserSessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_to root_path, success: t('.success')
+    else
+      flash.now[:error] = t('.fail')
+      render :new
+    end
+  end
+end
