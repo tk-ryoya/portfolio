c77c88ceed6a1c3f87bf3e507ca0bd6744ea322e
takeda_ryoya <t.ryooya@gmail.com>
2022-08-23 14:33:14 +0900

    Add: reservationモデルにバリデーションを追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
 
   enum reservation_type: { first: 0, repeate: 1}
+
+  validates :reservation_type, presence: true
+  validates :reservation_datetime, presence: true
 end
