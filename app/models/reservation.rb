109831bdd23989eee3c295c7c56945dfc613e071
takeda_ryoya <t.ryooya@gmail.com>
2022-08-26 15:15:25 +0900

    Fix: 別のenumと競合してしまったため定義を修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
 
-  enum reservation_type: { first: 0, repeate: 1}
+  enum reservation_type: { first_interview: 0, repeate_interview: 1 }
 
   validates :reservation_type, presence: true
   validates :reservation_datetime, presence: true
