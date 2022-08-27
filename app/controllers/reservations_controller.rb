bdd726133a1604ba59dbad693d889eab6a1d3896
takeda_ryoya <t.ryooya@gmail.com>
2022-08-27 21:40:16 +0900

    Add: reservations_controllerのCRUD機能を追加

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -22,8 +22,39 @@ class ReservationsController < ApplicationController
   end
 
   def index
+    @reservations = Reservation.all
   end
 
   def new
+    @reservation = current_user.reservations.build
+  end
+
+  def create
+    @reservation = current_user.reservations.build(reservation_params)
+    if @reservation.save
+      redirect_to reservations_path, '予約しました'
+    else
+      flash.now[:error] = '予約に失敗しました'
+    end
+  end
+
+  def show
+    @reservation = current_user.reservations.find(params[:id])
+  end
+
+  def edit
+    @reservation = current_user.reservations.find(params[:id])
+  end
+
+  def destroy
+    @reservation = current_user.reservations.find(params[:id])
+    @reservation.destroy!
+    redirect_to reservations_path, success: '予約キャンセルしました'
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :reservation_type, :reservation_datetime)
   end
 end
