039ad36edd42bfab96e31078dbe1386dcbbe6496
takeda_ryoya <t.ryooya@gmail.com>
2022-08-29 13:41:09 +0900

    Remove&Fix: google_calendar_controllerを削除、GoogleCalendarの認証コードをモデルに切り出す修正

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,26 +1,4 @@
 class ReservationsController < ApplicationController
-  require 'google/apis/calendar_v3'
-  require 'googleauth'
-
-  APPLICATION_NAME = 'GOOGLE_CALENDAR_EXAMPLE'.freeze
-  MY_CALENDAR_ID = '3luqpqlml35q3ki2kb7tk4hqbk@group.calendar.google.com'.freeze
-  CLIENT_SECRET_PATH = './service_account_json_key.json'.freeze
-
-  def initialize
-    @service = Google::Apis::CalendarV3::CalendarService.new
-    @service.client_options.application_name = APPLICATION_NAME
-    @service.authorization = authorize
-    @calendar_id = MY_CALENDAR_ID
-  end
-
-  def authorize
-    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: File.open(CLIENT_SECRET_PATH),
-      scope: Google::Apis::CalendarV3::AUTH_CALENDAR)
-    authorizer.fetch_access_token!
-    authorizer
-  end
-
   def index
     @reservations = Reservation.all
   end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,4 +5,35 @@ class Reservation < ApplicationRecord
 
   validates :reservation_type, presence: true
   validates :reservation_datetime, presence: true
+
+  APPLICATION_NAME = 'GOOGLE_CALENDAR_EXAMPLE'.freeze
+  MY_CALENDAR_ID = '3luqpqlml35q3ki2kb7tk4hqbk@group.calendar.google.com'.freeze
+  CLIENT_SECRET_PATH = './service_account_json_key.json'.freeze
+
+  def initialize
+    @service = Google::Apis::CalendarV3::CalendarService.new
+    @service.client_options.application_name = APPLICATION_NAME
+    @service.authorization = authorize
+    @calendar_id = MY_CALENDAR_ID
+  end
+
+  def authorize
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(CLIENT_SECRET_PATH),
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR)
+    authorizer.fetch_access_token!
+    authorizer
+  end
+
+  # だいそんさんの認証用コード
+  # def initialize
+  #   @calendar_service = Google::Apis::CalendarV3::CalendarService.new.tap { |cs| cs.authorization = authorizer }
+  # end
+  #
+  # def authorizer
+  #   @authorizer ||= Google::Auth::ServiceAccountCredentials.make_creds(
+  #     json_key_io: File.open('credentials.json'),
+  #     scope: scope
+  #   )
+  # end
 end
