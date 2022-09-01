2752adfb313aa7be408000693fb9ca6125eb4f25
takeda_ryoya <t.ryooya@gmail.com>
2022-09-01 11:12:44 +0900

    Add: deleteアクション用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
-  get    'login',  to: 'user_sessions#new'
-  post   'login',  to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
+  get    '/login',  to: 'user_sessions#new'
+  post   '/login',  to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
 
-  get 'oauth2callback', to: 'calendars#callback'
+  get '/oauth2callback', to: 'calendars#callback'
 
   resources :users, only: %i[new create]
-  resources :reservations
+  resources :reservations, only: [:index, :new, :create, :edit, :show]
+  delete '/reservations/:id', to: 'reservations#delete'
 end
