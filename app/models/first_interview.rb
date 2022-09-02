e049f401c41e6e73c0c2d4501ff1323610c28224
takeda_ryoya <t.ryooya@gmail.com>
2022-09-02 15:18:55 +0900

    Add: 再診予約に使用する負傷部位用のenumを追加

--- app/models/first_interview.rb
+++ app/models/first_interview.rb
@@ -1,6 +1,14 @@
 class FirstInterview < ApplicationRecord
   has_many :reservation_interviews, as: :interviewable
 
+  enum injury_position: {
+    neck_shoulder: 0,
+    waist_back: 1,
+    arm_hand: 2,
+    chest_abdomen: 3,
+    leg: 4
+  }
+
   validates :injury_date, presence: true
   validates :injury_position, presence: true
   validates :description, length: { maximum: 255 }
