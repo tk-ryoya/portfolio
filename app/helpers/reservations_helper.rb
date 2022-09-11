6bb43b7ec98f2709a8b22e5d9c177aba1d870dc3
takeda_ryoya <t.ryooya@gmail.com>
2022-09-11 15:52:04 +0900

    Add: 予約を取得するロジックを追加

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -7,4 +7,19 @@ module ReservationsHelper
              "17:00",
              "18:00"]
   end
+
+  def check_reservation(reservations, day, time)
+    result = false
+    reservations_count = reservations.count
+    if reservations_count > 1
+      reservations.each do |reservation|
+        result = reservation[:reservation_date].eql?(day.strftime("%Y-%m-%d")) && reservation[:reservation_time].eql?(time)
+        return result if result
+      end
+    elsif reservations_count == 1
+      result = reservations[0][:reservation_date].eql?(day.strftime("%Y-%m-%d")) && reservations[0][:reservation_time].eql?(time)
+      return result if result
+    end
+    return result
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -11,15 +11,26 @@ class Reservation < ApplicationRecord
 
   now = Time.current
 
-  scope :reservations_09_10, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,9,0,0)..Time.new(now.year,now.month,now.day,10,0,0)) }
-  scope :reservations_10_11, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,10,0,0)..Time.new(now.year,now.month,now.day,11,0,0)) }
-  scope :reservations_11_12, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,11,0,0)..Time.new(now.year,now.month,now.day,12,0,0)) }
-  scope :reservations_16_17, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,16,0,0)..Time.new(now.year,now.month,now.day,17,0,0)) }
-  scope :reservations_17_18, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,17,0,0)..Time.new(now.year,now.month,now.day,18,0,0)) }
-  scope :reservations_18_19, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,18,0,0)..Time.new(now.year,now.month,now.day,19,0,0)) }
+  scope :reservations_09_10, -> { where(start_time: Time.new(now.year,now.month,now.day,9,0,0)..Time.new(now.year,now.month,now.day,10,0,0)) }
+  # scope :reservations_10_11, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,10,0,0)..Time.new(now.year,now.month,now.day,11,0,0)) }
+  # scope :reservations_11_12, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,11,0,0)..Time.new(now.year,now.month,now.day,12,0,0)) }
+  # scope :reservations_16_17, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,16,0,0)..Time.new(now.year,now.month,now.day,17,0,0)) }
+  # scope :reservations_17_18, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,17,0,0)..Time.new(now.year,now.month,now.day,18,0,0)) }
+  # scope :reservations_18_19, -> { where(reservation_datetime: Time.new(now.year,now.month,now.day,18,0,0)..Time.new(now.year,now.month,now.day,19,0,0)) }
 
   def delete!
     self.deleted_at = Time.now
     save!
+  end
+
+  def self.reservations_after_three_month
+    reservations = Reservation.all.where("reservation_date >= ?", Date.current).where("reservation_date < ?", Date.current >> 3).order(reservation_date: :desc)
+    reservation_data = []
+    reservations.each do |reservation|
+      reservation_hash = {}
+      reservation_hash.merge!(day: reservation.reservation_date.strftime("%Y-%m-%d"), time: reservation.reservation_time)
+      reservation_data.push(reservation_hash)
+    end
+    reservation_data
   end
 end
