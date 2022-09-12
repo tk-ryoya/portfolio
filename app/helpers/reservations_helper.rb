eca3617848fba1c75d621b7d19f6432202bf5e00
takeda_ryoya <t.ryooya@gmail.com>
2022-09-12 16:18:06 +0900

    Fix: 土曜日PMと日曜日AMPMは予約カレンダーから予約できないように修正

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,9 +1,12 @@
 module ReservationsHelper
-  def times
+  def am_times
     times = ["9:00",
              "10:00",
-             "11:00",
-             "16:00",
+             "11:00"]
+  end
+
+  def pm_times
+    times = ["16:00",
              "17:00",
              "18:00"]
   end
