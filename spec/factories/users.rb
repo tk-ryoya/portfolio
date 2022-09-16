013a92eaca233055a17e28a7fc72b2d26035faef
takeda_ryoya <t.ryooya@gmail.com>
2022-09-16 15:52:29 +0900

    Add: ユーザーのバリデーションテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    last_name { "滋賀" }
+    first_name { "琵琶子" }
+    last_name_kana { "シガ" }
+    first_name_kana { "ビワコ" }
+    tel { "12345678910" }
+    sequence(:email) { |n| "user_#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
