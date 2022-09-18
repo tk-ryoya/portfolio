c18076ad35675c4c612c1254805ee286bf1e4d2a
takeda_ryoya <t.ryooya@gmail.com>
2022-09-18 18:25:10 +0900

    Add: 予約完了後、GoogleCalendarへ予約情報を登録する機能を追加

--- app/controllers/repeate_interviews_controller.rb
+++ app/controllers/repeate_interviews_controller.rb
@@ -7,8 +7,13 @@ class RepeateInterviewsController < ApplicationController
   def create
     @repeate_interview = RepeateInterview.create(repeate_interview_params)
     @repeate_reservation = @repeate_interview.reservation_interviews.build(reservation_id: params[:reservation_id])
+    reservation = Reservation.find(params[:reservation_id])
+    reservation_start_time = reservation.start_time
+    reservation_end_time = reservation_start_time + 60 * 60
+    calendar = Calendar.new
     if @repeate_reservation.save
       redirect_to reservations_path, success: t('.success')
+      calendar.set_event("#{current_user.decorate.full_name}" + "  " + "(再診)", reservation_start_time, reservation_end_time)
     else
       flash.now[:error] = t('.fail')
       render :new
