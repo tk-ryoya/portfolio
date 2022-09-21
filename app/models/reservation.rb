4be9bbc1ffd3e4b5d0dba8ed852a34bc59618780
takeda_ryoya <t.ryooya@gmail.com>
2022-09-21 14:16:56 +0900

    Fix: 当日の予約、午後休診(臨時)に予約の際は電話で対応するように修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -12,8 +12,12 @@ class Reservation < ApplicationRecord
   def self.check_reservation_day(day)
     if day < Date.current
       return '過去の日付は選択できません'
-    elsif BusinessCalendar.temporary_closed_day?(day) #|| BusinessCalendar.temporary_closed_day_pm?(day)
+    elsif day < (Date.current + 1)
+      return '当日予約は電話にてご連絡ください'
+    elsif BusinessCalendar.temporary_closed_day?(day)
       return '臨時休診のため選択できません'
+    elsif BusinessCalendar.temporary_closed_day_pm?(day)
+      return '午後休診(臨時)のため、電話にてお問い合わせください'
     end
   end
 
