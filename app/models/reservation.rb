b70e8471657d5f64ab100a5117d2f94de593b69f
takeda_ryoya <t.ryooya@gmail.com>
2022-09-22 09:00:10 +0900

    Add: 予約エラー処理を一部追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,6 +14,8 @@ class Reservation < ApplicationRecord
       return '過去の日付は選択できません'
     elsif day < (Date.current + 1)
       return '当日予約は電話にてご連絡ください'
+    elsif (Date.current >> 3) < day
+      return '3ヶ月以降先の予約は電話にてご連絡ください'
     elsif BusinessCalendar.temporary_closed_day?(day)
       return '臨時休診のため選択できません'
     elsif BusinessCalendar.temporary_closed_day_pm?(day)
