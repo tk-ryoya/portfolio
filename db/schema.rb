5d9458a983b47f8c71c820fa0cb446f32770894c
takeda_ryoya <t.ryooya@gmail.com>
2022-09-22 19:06:35 +0900

    Add: sorceryのreset_passwordをインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_021836) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_095521) do
   create_table "first_interviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.date "injury_date"
     t.integer "injury_position", default: 0, null: false
@@ -60,7 +60,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_16_021836) do
     t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "reservation_interviews", "reservations"
