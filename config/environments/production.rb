3af7b6cb3732596739c8f38aa635767c5a1113a1
takeda_ryoya <t.ryooya@gmail.com>
2022-09-26 14:06:42 +0900

    Fix: 本番環境のメイラーURLを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "reservation_service_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {  host: 'https://tk-ryoya-reservation-service.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
