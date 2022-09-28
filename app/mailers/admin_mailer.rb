8b115eecf12da578abc6024bae343525a90649b3
takeda_ryoya <t.ryooya@gmail.com>
2022-09-28 14:55:43 +0900

    Fix: メイラーの送信先を編集

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,6 +4,6 @@ class AdminMailer < ApplicationMailer
     wd = ["日", "月", "火", "水", "木", "金", "土"]
     @reservation_time = reservation.start_time.strftime("%m月%d日(#{wd[reservation.start_time.wday]}) %H:%M")
     @user_name = user.decorate.full_name
-    mail from: ENV['MAIL_ADDRESS'], to: ENV['ADMIN_MAIL_ADDRESS'], subject: '予約が入りました'
+    mail from: ENV['MAIL_ADDRESS'], to: ENV['HOSPITAL_DIRECTOR_MAIL_ADDRESS'], bcc: ENV['ADMIN_MAIL_ADDRESS'], subject: '予約が入りました'
   end
 end
