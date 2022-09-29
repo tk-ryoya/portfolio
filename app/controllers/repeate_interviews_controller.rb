95f54efee26d009bad3c2d4da62dc64f2c70e64b
takeda_ryoya <t.ryooya@gmail.com>
2022-09-29 21:00:42 +0900

    Fix: GoogleCalendarへの予約情報登録を一旦停止

--- app/controllers/repeate_interviews_controller.rb
+++ app/controllers/repeate_interviews_controller.rb
@@ -7,13 +7,13 @@ class RepeateInterviewsController < ApplicationController
   def create
     @repeate_interview = RepeateInterview.create(repeate_interview_params)
     @repeate_reservation = @repeate_interview.reservation_interviews.build(reservation_id: params[:reservation_id])
-    reservation = Reservation.find(params[:reservation_id])
-    reservation_start_time = reservation.start_time
-    reservation_end_time = reservation_start_time + 1.hours
-    calendar = Calendar.new
+    # reservation = Reservation.find(params[:reservation_id])
+    # reservation_start_time = reservation.start_time
+    # reservation_end_time = reservation_start_time + 1.hours
+    # calendar = Calendar.new
     if @repeate_reservation.save
       redirect_to reservations_path, success: t('.success')
-      calendar.set_event("再診予約", "予約番号:  " + "「#{reservation.id}」", reservation_start_time, reservation_end_time)
+      # calendar.set_event("再診予約", "予約番号:  " + "「#{reservation.id}」", reservation_start_time, reservation_end_time)
       AdminMailer.reservation_notification(reservation, current_user).deliver_now
     else
       flash.now[:error] = t('.fail')
