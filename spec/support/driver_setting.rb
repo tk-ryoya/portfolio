224de0ff1938b323014789e013375f85e0264dc4
takeda_ryoya <t.ryooya@gmail.com>
2022-10-05 14:47:00 +0900

    Add: ユーザー新規登録のテストコードを追加

--- spec/support/driver_setting.rb
+++ spec/support/driver_setting.rb
@@ -1,11 +1,5 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by(:selenium_chrome_headless)
-  end
-end
-
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome
   end
 end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    describe 'ユーザー新規登録' do
+      context 'フォームの入力値が正常' do
+        it 'ユーザーの新規作成が成功する' do
+          visit new_user_path
+          fill_in '姓', with: '滋賀'
+          fill_in '名', with: '琵琶子'
+          fill_in '姓(フリガナ)', with: 'シガ'
+          fill_in '名(フリガナ)', with: 'ビワコ'
+          fill_in '電話番号(ハイフン無し)', with: '12345678910'
+          fill_in 'メールアドレス', with: 'biwako@example.com'
+          fill_in 'パスワード', with: 'password'
+          fill_in 'パスワード確認', with: 'password'
+          click_button '新規登録'
+          expect(page).to have_content '新規登録が完了しました'
+          expect(current_path).to eq login_path
+        end
+      end
+
+      context 'メールアドレスが未入力' do
+        it 'ユーザーの新規登録が失敗する' do
+          visit new_user_path
+          fill_in '姓', with: '滋賀'
+          fill_in '名', with: '琵琶子'
+          fill_in '姓(フリガナ)', with: 'シガ'
+          fill_in '名(フリガナ)', with: 'ビワコ'
+          fill_in '電話番号(ハイフン無し)', with: '12345678910'
+          fill_in 'メールアドレス', with: ''
+          fill_in 'パスワード', with: 'password'
+          fill_in 'パスワード確認', with: 'password'
+          click_button '新規登録'
+          expect(page).to have_content '新規登録に失敗しました'
+          expect(page).to have_content 'メールアドレスを入力してください'
+          expect(current_path).to eq users_path
+        end
+      end
+
+      context '登録済みのメールアドレスを使用' do
+        it 'ユーザーの新規登録が失敗する' do
+          existed_user = create(:user)
+          visit new_user_path
+          fill_in '姓', with: '滋賀'
+          fill_in '名', with: '琵琶子'
+          fill_in '姓(フリガナ)', with: 'シガ'
+          fill_in '名(フリガナ)', with: 'ビワコ'
+          fill_in '電話番号(ハイフン無し)', with: '12345678910'
+          fill_in 'メールアドレス', with: existed_user.email
+          fill_in 'パスワード', with: 'password'
+          fill_in 'パスワード確認', with: 'password'
+          click_button '新規登録'
+          expect(page).to have_content '新規登録に失敗しました'
+          expect(page).to have_content 'メールアドレスはすでに存在します'
+          expect(current_path).to eq users_path
+        end
+      end
+    end
+  end
+end
