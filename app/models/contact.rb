7f643305f2b445ed74c5b0b8231e88b2a5c187c9
takeda_ryoya <t.ryooya@gmail.com>
2022-10-06 09:30:34 +0900

    Add: 電話番号とメールアドレスのバリデーションを追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true
   validates :content, presence: true, length: { maximum: 500 }
 end
