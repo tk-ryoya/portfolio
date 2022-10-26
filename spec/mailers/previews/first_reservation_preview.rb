74bdd15606eb13f88482d2dc43528b44034e53f5
takeda_ryoya <t.ryooya@gmail.com>
2022-10-26 10:35:17 +0900

    Add: 予約詳細を含むメイラーのテストコードを追加

---
+++ spec/mailers/previews/first_reservation_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/first_reservation
+class FirstReservationPreview < ActionMailer::Preview
+
+end
---
+++ spec/mailers/repeate_reservation_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe RepeateReservationMailer, type: :mailer do
+  describe '管理者へ予約完了の通知メール' do
+    let(:user) { create(:user) }
+    let(:reservation) { create(:reservation, user: user) }
+    let(:repeate_interview) { create(:repeate_interview) }
+    let(:mail) { RepeateReservationMailer.reservation_notification(reservation, user, repeate_interview.description) }
+    wd = ["日", "月", "火", "水", "木", "金", "土"]
+
+    it '送信用のメールアドレスから送信されていること' do
+      expect(mail.from).to eq [ENV['MAIL_ADDRESS']]
+    end
+
+    it '管理者のメールアドレスに送信されていること' do
+      expect(mail.to).to eq [ENV['HOSPITAL_DIRECTOR_MAIL_ADDRESS']]
+    end
+
+    it '管理者のメールアドレスに送信されていること' do
+      expect(mail.bcc).to eq [ENV['ADMIN_MAIL_ADDRESS']]
+    end
+
+    it '正しい件名で送信されていること' do
+      expect(mail.subject).to eq '予約が入りました'
+    end
+
+    it 'メール本文にユーザーのフルネームが表示されていること' do
+      expect(mail.html_part.body.to_s).to match(user.decorate.full_name)
+    end
+
+    it 'メール本文に予約日時が表示されていること' do
+      expect(mail.html_part.body.to_s).to include(reservation.start_time.strftime("%m月%d日(#{wd[reservation.start_time.wday]}) %H:%M"))
+    end
+
+    it 'メール本文に予約詳細が表示されていること' do
+      expect(mail.html_part.body.to_s).to match(repeate_interview.description)
+    end
+  end
+end
