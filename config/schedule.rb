f03bb068e39499bd644336e08e132208c48386fa
takeda_ryoya <t.ryooya@gmail.com>
2023-03-05 18:58:20 +0900

    Fix: wheneverの設定を本番環境に変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment") # Rails.root(Railsメソッド)を使用するために必要
-rails_env = ENV['RAILS_ENV'] || :development # cronを実行する環境変数(:development, :product, :test)
+rails_env = ENV['RAILS_ENV'] || :production # cronを実行する環境変数(:development, :product, :test)
 set :environment, rails_env # cronを実行する環境変数をセット
 set :output, "#{Rails.root}/log/crontab.log" # cronのログ出力用ファイル
 
