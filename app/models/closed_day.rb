44ae3f5877a0a0600f3406885b373618916e6965
takeda_ryoya <t.ryooya@gmail.com>
2023-03-15 17:05:43 +0900

    Fix: 臨時午後休診の「休」表示に関するエラー修正

--- app/models/closed_day.rb
+++ app/models/closed_day.rb
@@ -2,4 +2,16 @@ class ClosedDay < ApplicationRecord
   def self.schedules
     ClosedDay.pluck(:date)
   end
+
+  def self.closed_am
+    date = ClosedDay.all
+    closed_am = date.map { |d| d.date if d.closed_days }
+    closed_am.compact
+  end
+
+  def self.closed_pm
+    date = ClosedDay.all
+    closed_pm = date.map { |d| d.date if d.closed_days_pm }
+    closed_pm.compact
+  end
 end
